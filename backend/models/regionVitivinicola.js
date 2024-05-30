2e51968cd0540844b44fbd19ece7e50f560db0a9
Tomas Tealdi <tomastealdi04@gmail.com>
2024-05-29 21:45:02 -0300

    Third commit

--- backend/models/regionVitivinicola.js
+++ backend/models/regionVitivinicola.js
@@ -24,6 +24,7 @@ class RegionVitivinicola {
   // la region no conoce a que provincia pertenece, pero las provincias si conocen que regiones tienen,  además los paises tienen provincias, las provincias tienen un método obtenerPias() que devuelve el país, quiero obtener el pais de la región.
   obtenerPais(provincias, paises) {
     console.log("hola");
+
     let provincia = provincias.find((provincia) =>
       provincia.regionesVitivinicolas.includes(this)
     );
